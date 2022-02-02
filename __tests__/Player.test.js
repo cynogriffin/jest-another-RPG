72eb71f55b2722c7b2949cd465061a502109ca83
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-02-01 23:30:59 -0600

    added base functionality for the Game object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,7 @@
-const { test, expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
